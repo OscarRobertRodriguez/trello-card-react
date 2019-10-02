ec9169d8f0b08602046e99d43246a113b52c5ffb
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2019-10-02 12:38:49 -0500

    Updates

---
+++ precache-manifest.e2a8e1367e4c8639a11ceed1623c1074.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb0e4dd14fe84e9784768a0d9baeb0e",
+    "url": "/trello-card-react/index.html"
+  },
+  {
+    "revision": "5dc9a26da1143526514e",
+    "url": "/trello-card-react/static/css/main.f7be724f.chunk.css"
+  },
+  {
+    "revision": "e3a5cc65063f842d8ba4",
+    "url": "/trello-card-react/static/js/2.8c917dba.chunk.js"
+  },
+  {
+    "revision": "5dc9a26da1143526514e",
+    "url": "/trello-card-react/static/js/main.e5d52fa5.chunk.js"
+  },
+  {
+    "revision": "e11dd18b637d082c1613",
+    "url": "/trello-card-react/static/js/runtime-main.5014ce73.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8fbb7444af856405edf132a3c4551c5f.js"
+  "/trello-card-react/precache-manifest.e2a8e1367e4c8639a11ceed1623c1074.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trello-card-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
